27170463c84de33e66f4bf500a4d3e4648321753
harish-k05 <harishkece2026@gmail.com>
2025-04-14 00:59:07 +0530

    setting up for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
-   base: '/ESTATE-PROJECT/'
+   base: '/ESTATE-PROJECT/', 
+   build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
 })
 
